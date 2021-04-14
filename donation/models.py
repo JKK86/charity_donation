cd843b504ac279f3fe9599b560b9938334d47dbf
JKK86 <j.kurowski86@wp.pl>
2021-04-14 22:58:37 +0200

    add requirements.txt

--- donation/models.py
+++ donation/models.py
@@ -54,7 +54,7 @@ class Donation(models.Model):
     pick_up_date = models.DateField(verbose_name="Data odbioru")
     pick_up_time = models.TimeField(verbose_name="Godzina odbioru")
     pick_up_comment = models.TextField(verbose_name="Komentarz", null=True, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, default=True,
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, default=None,
                              verbose_name="Użytkownik")
     is_taken = models.BooleanField(verbose_name="Stan", default=False)
 
